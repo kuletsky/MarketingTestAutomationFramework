ecea10e2d8649b19430e8da23601cbcf403b30fa
kuletsky <kuletsky@gmail.com>
2025-10-21 22:32:46 -0400

    add MenuComponent

---
+++ src/test/java/com/empower/components/MenuComponent.java
@@ -0,0 +1,12 @@
+package com.empower.components;
+
+import org.openqa.selenium.WebDriver;
+
+public class MenuComponent extends HeaderComponent {
+
+    public MenuComponent(WebDriver driver) {
+        super(driver);
+    }
+
+
+}

300c9e0c525d539d818707e74c80172737006d85
kuletsky <kuletsky@gmail.com>
2025-12-03 14:03:10 -0500

    new visual tests

--- src/test/java/com/empower/pages/base/BasePage.java
+++ src/test/java/com/empower/pages/base/BasePage.java
@@ -307,7 +307,7 @@ public class BasePage extends BaseModel{
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         wait.until(d -> ((JavascriptExecutor) d).executeScript("return document.readyState==='complete'").equals(true));
 
-        scrollToBottom();
+//        scrollToBottom();
 //        scrollToUp();
         // 2) Wait for an animation element to exist, then seek & pause to a deterministic frame
 //        new WebDriverWait(driver, Duration.ofSeconds(60)).until(d ->
